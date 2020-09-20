d8ead08fd07af4dbfe2cc5bf37ecc6326f3c4ae4
Bruno Egremy <egremy.bruno@gmail.com>
2020-09-20 14:45:28 +0200

    Added TailwindCSS Node module

--- site/html/helloworld.php
+++ site/html/helloworld.php
@@ -1,10 +1,19 @@
 <html>
  <head>
   <title>PHP Test</title>
+
+  <link rel="stylesheet" href="../css/output.css">
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; 
- $version = phpversion();
- print $version; ?>
+     <?php echo '<p>Hello World</p>';
+     $version = phpversion();
+     print $version; ?>
+
+     <br>
+
+     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+         TailwindCSS Button
+     </button>
+
  </body>
 </html>
