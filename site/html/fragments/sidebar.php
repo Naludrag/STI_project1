31c5f44de9384b709494aa47816f9a3024af8f2b
b3cauda <arthur.becaud@heig-vd.ch>
2020-09-21 16:28:29 +0200

    Add link to admin.php page in sidebar

--- site/html/fragments/sidebar.php
+++ site/html/fragments/sidebar.php
@@ -14,7 +14,7 @@
             </li>
             <?php if ($_SESSION['admin'] == 1): ?>
                 <li class="mr-3 flex-1">
-                    <a href="#" class="block py-1 md:py-3 pl-1 align-middle text-gray-800 no-underline hover:text-indigo-600 border-b-2 border-gray-800 md:border-gray-900 hover:border-indigo-600">
+                    <a href="admin.php" class="block py-1 md:py-3 pl-1 align-middle text-gray-800 no-underline hover:text-indigo-600 border-b-2 border-gray-800 md:border-gray-900 hover:border-indigo-600">
                         <i class="fas fa-users pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Manage Users</span>
                     </a>
                 </li>
